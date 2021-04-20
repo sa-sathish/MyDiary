85a0e0bcabc561d73d6aa0eb9ba65d6050ea4d78
Sathish_Work <sa.shanmugam@tawari.sa>
2021-04-20 15:10:51 +0300

    Initial Commit

---
+++ MyDiary/MyDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiaryApp.swift
+//  MyDiary
+//
+//  Created by sathish s on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
